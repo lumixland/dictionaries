3bb08b2a59f1aa67f6a4bede6a2ff4f22da405d6
Jonas Franke <jonasfranke@sdevs.org>
2024-12-25 13:53:39 +0100

    Update README files and format TypeScript interface for localization

--- website/lang.d.ts
+++ website/lang.d.ts
@@ -1,30 +1,111 @@
 export interface LangContent {
-    index: {
-      verifiedBot: string;
+  index: {
+    verifiedBot: string;
+    title: string;
+    description: string;
+    install: string;
+    learnMore: string;
+    trustedBy: string;
+    members: string;
+    runAnimation: string;
+    playingAnimation: string;
+  };
+  "not-found": {
+    title: string;
+    description: string;
+    back: string;
+    support: string;
+  };
+  error: {
+    title: string;
+    description: string;
+    tryAgain: string;
+    support: string;
+  };
+  tutorials: {
+    title: string;
+    categories: {
+      tutorialTypes: {
+        SERVER_MANAGEMENT: string;
+        ECONOMY: string;
+        MODERATION: string;
+        FUN: string;
+        MUSIC: string;
+        LEVELING: string;
+        WELCOME: string;
+        GOODBYE: string;
+        REACTION_ROLES: string;
+        TICKET: string;
+        SUGGESTION: string;
+        REPORT: string;
+        ANNOUNCEMENT: string;
+        POLL: string;
+        GIVEAWAY: string;
+        SHOP: string;
+        BOOSTER: string;
+        JOB: string;
+      };
+    };
+    noResults: string;
+  };
+  tutorial: {
+    lastUpdated: string;
+    edit: string;
+    onThisPage: string;
+  };
+  premium: {
+    title: string;
+    description: string;
+    save: string;
+    perMonth: string;
+    subscribe: string;
+    subscribedTo: string;
+    plans: {
+      monthly: {
+        name: string;
+        billedAt: string;
+        features: {
+          "Higher-message-limit": string;
+          "Custom-emojis": string;
+          "Priority-support": string;
+          "Access-to-beta-features": string;
+        };
+      };
+      yearly: {
+        name: string;
+        billedAt: string;
+        features: {
+          "Higher-message-limit": string;
+          "Custom-emojis": string;
+          "Priority-support": string;
+          "Access-to-beta-features": string;
+        };
+      };
+    };
+  };
+  dashboard: {
+    "server-select": {
       title: string;
       description: string;
-      install: string;
-      learnMore: string;
-      trustedBy: string;
-      members: string;
-      runAnimation: string;
-      playingAnimation: string;
+      manage: string;
+      setup: string;
     };
-    "not-found": {
+  };
+  modals: {
+    leavingLumix: {
       title: string;
       description: string;
-      back: string;
-      support: string;
+      cancel: string;
+      continue: string;
     };
-    error: {
-      title: string;
-      description: string;
-      tryAgain: string;
-      support: string;
+    imageView: {
+      close: string;
     };
-    tutorials: {
-      title: string;
-      categories: {
+    editTutorial: {
+      tabs: {
+        edit: string;
+        titlePlaceholder: string;
+        topicsPlaceholder: string;
         tutorialTypes: {
           SERVER_MANAGEMENT: string;
           ECONOMY: string;
@@ -45,124 +126,43 @@ export interface LangContent {
           BOOSTER: string;
           JOB: string;
         };
+        preview: string;
       };
-      noResults: string;
-    };
-    tutorial: {
-      lastUpdated: string;
-      edit: string;
-      onThisPage: string;
-    };
-    premium: {
-      title: string;
-      description: string;
       save: string;
-      perMonth: string;
-      subscribe: string;
-      subscribedTo: string;
-      plans: {
-        monthly: {
-          name: string;
-          billedAt: string;
-          features: {
-            "Higher-message-limit": string;
-            "Custom-emojis": string;
-            "Priority-support": string;
-            "Access-to-beta-features": string;
-          };
-        };
-        yearly: {
-          name: string;
-          billedAt: string;
-          features: {
-            "Higher-message-limit": string;
-            "Custom-emojis": string;
-            "Priority-support": string;
-            "Access-to-beta-features": string;
-          };
-        };
-      };
+      cancel: string;
     };
-    dashboard: {
-      "server-select": {
-        title: string;
-        description: string;
-        manage: string;
-        setup: string;
-      };
-    };
-    modals: {
-      leavingLumix: {
-        title: string;
-        description: string;
-        cancel: string;
-        continue: string;
-      };
-      imageView: {
-        close: string;
-      };
-      editTutorial: {
-        tabs: {
-          edit: string;
-          titlePlaceholder: string;
-          topicsPlaceholder: string;
-          tutorialTypes: {
-            SERVER_MANAGEMENT: string;
-            ECONOMY: string;
-            MODERATION: string;
-            FUN: string;
-            MUSIC: string;
-            LEVELING: string;
-            WELCOME: string;
-            GOODBYE: string;
-            REACTION_ROLES: string;
-            TICKET: string;
-            SUGGESTION: string;
-            REPORT: string;
-            ANNOUNCEMENT: string;
-            POLL: string;
-            GIVEAWAY: string;
-            SHOP: string;
-            BOOSTER: string;
-            JOB: string;
-          };
-          preview: string;
-        };
-        save: string;
-        cancel: string;
-      };
-    };
-    "footer-banner": {
-      title: string;
-      description: string;
-      install: string;
-      majoexe: string;
-    };
-    footer: {
-      resources: string;
-      github: string;
-      tutorials: string;
-      support: string;
-      dashboard: string;
-      premium: string;
-      install: string;
-      legal: string;
-      privacyPolicy: string;
-      termsOfService: string;
-      allRightsReserved: string;
-      empowerDescription: string;
-      servicesOperational: string;
-      servicesDown: string;
-    };
-    header: {
-      features: string;
-      servers: string;
-      premium: string;
-      login: string;
-      logout: string;
-      myServers: string;
-      transferPremium: string;
-      supportServer: string;
-      tutorials: string;
-    };
+  };
+  "footer-banner": {
+    title: string;
+    description: string;
+    install: string;
+    majoexe: string;
+  };
+  footer: {
+    resources: string;
+    github: string;
+    tutorials: string;
+    support: string;
+    dashboard: string;
+    premium: string;
+    install: string;
+    legal: string;
+    privacyPolicy: string;
+    termsOfService: string;
+    allRightsReserved: string;
+    empowerDescription: string;
+    servicesOperational: string;
+    servicesDown: string;
+  };
+  header: {
+    features: string;
+    servers: string;
+    premium: string;
+    login: string;
+    logout: string;
+    myServers: string;
+    transferPremium: string;
+    supportServer: string;
+    tutorials: string;
+  };
 }
